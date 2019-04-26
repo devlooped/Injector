827babf0f7be2ead235fb928ea18b475b3297ad9
Daniel Cazzulino <daniel@cazzulino.com>
2019-04-26 20:04:18 -0300

    Provide an actual useful example of how to use
    With ready F5 support to test too.

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Sample
 {
@@ -10,6 +9,34 @@ namespace Sample
     {
         static void Main(string[] args)
         {
+            var exe = args.FirstOrDefault() ?? @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe";
+
+            var process = Process.Start(exe);
+
+            process.WaitForInputIdle();
+
+            // Inspect %LocalAppData%\Windows.Injector\log.txt to troubleshoot
+
+            Bootstrap.Injector.Launch(
+                process.MainWindowHandle, 
+                Assembly.GetExecutingAssembly().Location, 
+                typeof(Startup).FullName, 
+                $"{nameof(Startup.Start)}:hello:42:true");
+
+            Console.ReadLine();
+            
+            if (!process.HasExited)
+                process.Kill();
+        }
+    }
+
+    public static class Startup
+    {
+        // NOTE: parameter type conversion from the {method}:arg1:argN format is done automatically
+        public static void Start(string arg1, int arg2, bool debug)
+        {
+            if (debug)
+                Debugger.Launch();
         }
     }
 }
