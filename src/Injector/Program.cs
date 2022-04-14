c18328c3242414fce9d46b7669d1c80b5d57def4
Daniel Cazzulino <daniel@cazzulino.com>
2022-04-13 22:52:42 -0300

    ⬆️ Update dependencies, build and pack

--- src/Injector/Program.cs
+++ src/Injector/Program.cs
@@ -3,23 +3,23 @@ using System.Diagnostics;
 
 namespace Injector
 {
-	/// <summary>
-	/// Helper process to allow injection into processes that have different bitness 
-	/// into WPF app processes.
-	///
-	/// VS is 32-bit, so it cannot write to the memory of a 64-bit process, and delegates
-	/// that to this program instead.
-	/// </summary>
-	public class Program
-	{
-		static void Main(string[] args)
-		{
-			var handle = new IntPtr(int.Parse(args[0]));
-			var supportDllPath = args[1].Trim('"');
-			var className = args[2].Trim('"');
-			var methodName = args[3].Trim('"');
+    /// <summary>
+    /// Helper process to allow injection into processes that have different bitness 
+    /// into WPF app processes.
+    ///
+    /// VS is 32-bit, so it cannot write to the memory of a 64-bit process, and delegates
+    /// that to this program instead.
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var handle = new IntPtr(int.Parse(args[0]));
+            var supportDllPath = args[1].Trim('"');
+            var className = args[2].Trim('"');
+            var methodName = args[3].Trim('"');
 
-			Bootstrap.Injector.Launch(handle, supportDllPath, className, methodName);
-		}
-	}
+            Bootstrap.Injector.Launch(handle, supportDllPath, className, methodName);
+        }
+    }
 }
--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -12,16 +12,14 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            var exe = args.FirstOrDefault() ?? @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe";
+            var exe = args.FirstOrDefault() ?? $"{ThisAssembly.Project.VsInstallRoot}\\devenv.exe";
 
             var process = Process.Start(exe);
             process.WaitForInputIdle();
             // NOTE: it's important to wait until the window handle is actually created. 
             // this may take a while if the startup of the target app is lenghty
             while (process.MainWindowHandle == IntPtr.Zero)
-            {
                 Thread.Sleep(200);
-            }
 
             // Inspect %LocalAppData%\Windows.Injector\log.txt to troubleshoot
 
@@ -30,21 +28,21 @@ namespace Sample
             NativeMethods.IsWow64Process(process.Handle, out var isWow);
             var platform = isWow ? "x86" : "x64";
 
-            Process.Start(Path.Combine("Injector", platform, "Injector.exe"), 
+            Process.Start(Path.Combine("Injector", platform, "Injector.exe"),
                 process.MainWindowHandle + " " +
                 Assembly.GetExecutingAssembly().Location + " " +
                 typeof(Startup).FullName + " " +
                 $"{nameof(Startup.Start)}:hello:42:true");
 
             // API-based injection if target process has same bitness as ours
-            Bootstrap.Injector.Launch(
-                process.MainWindowHandle,
-                Assembly.GetExecutingAssembly().Location,
-                typeof(Startup).FullName,
-                $"{nameof(Startup.Start)}:hello:42:true");
+            //Bootstrap.Injector.Launch(
+            //    process.MainWindowHandle,
+            //    Assembly.GetExecutingAssembly().Location,
+            //    typeof(Startup).FullName,
+            //    $"{nameof(Startup.Start)}:hello:42:true");
 
             Console.ReadLine();
-            
+
             if (!process.HasExited)
                 process.Kill();
         }
@@ -60,7 +58,7 @@ namespace Sample
         }
     }
 
-    internal static class NativeMethods
+    static class NativeMethods
     {
         [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
         [return: MarshalAs(UnmanagedType.Bool)]
